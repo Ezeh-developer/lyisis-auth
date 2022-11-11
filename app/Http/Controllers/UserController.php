a3a03a372777366d197dbd9f708fa046d027d01d
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-11 18:07:18 -0300

    errores de tipeo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,96 +2,104 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Database\QueryException;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
+use App\Http\Requests\CreateUserRequest;
 use App\Models\Client;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function create(CreateUserRequest $request) {
+    public function create(CreateUserRequest $request)
+    {
         try {
-            DB::transaction(function () use($request, $user, $client) {
+            DB::transaction(function () use ($request) {
                 Client::create([
                     'surname' => $request->post('surname'),
-                    'birth_date' => $request->post('birthDate')
+                    'birth_date' => $request->post('birthDate'),
                 ]);
                 User::create([
-                    'name' => $request -> post('name'),
-                    'email' => $request -> post('email'),
-                    'password' => Hash::make($request -> post('password'))
+                    'name' => $request->post('name'),
+                    'email' => $request->post('email'),
+                    'password' => Hash::make($request->post('password')),
                 ]);
                 $client->user()->save($user);
             });
-        }
-        catch (QueryException $e) {
+        } catch (QueryException $e) {
             return [
-                'result' => 'Unable to create user right now.'
+                'result' => 'Unable to create user right now.',
             ];
         }
 
         return [
-            'result' => 'User registered succesfully.'
+            'result' => 'User registered succesfully.',
         ];
     }
-    
-    public function authenticate(Request $request) {
-        $validation = $this->validateAuthentication($request);
-        if($validation !== true)
-            return $validation;
 
-        if(!Auth::attempt($request->only('email', 'password'), $request->post('remember')))
+    public function authenticate(Request $request)
+    {
+        $validation = $this->validateAuthentication($request);
+        if ($validation !== true) {
+            return $validation;
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->post('remember'))) {
             return [
-                'result' => 'Wrong password or email.'
+                'result' => 'Wrong password or email.',
             ];
+        }
 
         return [
             'result' => 'Succesfully logged in.',
             'subscriptionId' => User::firstWhere('id', Auth::id())->client->subscription_id,
-            'subscription' => Auth::user()->client->subscription->type
+            'subscription' => Auth::user()->client->subscription->type,
         ];
     }
 
     public function update(UpdateUserRequest $request)
     {
         try {
-            DB::transaction(function () use ($request){
+            DB::transaction(function () use ($request) {
                 User::find(Auth::id())->update([
                     'name' => $request->post('name'),
                     'email' => $request->post('email'),
                 ]);
-                if(!empty($request->post('password')))
+                if (!empty($request->post('password'))) {
                     User::find(Auth::id())->update([
-                        'password' => Hash::make($request->post('password'))
+                        'password' => Hash::make($request->post('password')),
                     ]);
+                }
+
                 User::find(Auth::id())->client->update([
                     'surname' => $request->post('surname'),
-                    'birth_date' => $request->post('birthdate')
+                    'birth_date' => $request->post('birthdate'),
                 ]);
-        });
+            });
         } catch (QueryExcpetion $e) {
             return [
-                'result' => 'Unable to update user right now.'
+                'result' => 'Unable to update user right now.',
             ];
         }
 
         return [
-            'result' => 'User information updated succesfully.'
+            'result' => 'User information updated succesfully.',
         ];
     }
 
-    private function validateAuthentication($request) {
-        $validator = Validator::make($request -> all(), [
+    private function validateAuthentication($request)
+    {
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email|exists:users',
             'password' => 'required',
-            'remember' => 'required|boolean'
+            'remember' => 'required|boolean',
         ]);
-        if($validator -> fails())
+        if ($validator->fails()) {
             return $validator->errors()->toJson();
+        }
 
         return true;
     }
